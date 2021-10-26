d1445cbaaddfd8e18d797bb7d40f702af321a95f
artem <zubov@lanit.ru>
2021-10-26 17:26:46 +0300

    Initial commit

---
+++ src/test/java/ru/lanit/SoapDemo/SoaPdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lanit.SoapDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaPdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
